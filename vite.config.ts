662a2d9aff58b26987765947d0d4ed7ff9a6c512
Mukisa Vaniah Christian <vierycalliper@gmail.com>
2025-11-21 15:11:59 +0300

    hide console logs in prod

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,9 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  esbuild: {
+    drop: mode === 'production' ? ['console', 'debugger'] : [],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
